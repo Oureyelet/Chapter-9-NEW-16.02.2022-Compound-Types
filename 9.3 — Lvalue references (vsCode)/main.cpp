be345fbbeef64871fcb1e8fa4690efd34854916a
Mateusz <oureyelet@gmail.com>
2022-02-18 16:23:10 +0000

    Lvalue references & Quiz Time

--- 9.3 — Lvalue references (vsCode)/main.cpp
+++ 9.3 — Lvalue references (vsCode)/main.cpp
@@ -201,14 +201,150 @@ int main()
     /*
     Reference variables follow the same scoping and duration rules that normal variables do:
     */
+    int i_One{ 2 }; // normal integer
+    int& ref_i_One{ i_One }; // reference to variable value
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "References and referents have independent lifetimes" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    With one exception (that we’ll cover next lesson), the lifetime of a reference and the lifetime of its referent are 
+    independent. In other words, both of the following are true:
+
+    A reference can be destroyed before the object it is referencing.
+    The object being referenced can be destroyed before the reference.
+
+    When a reference is destroyed before the referent, the referent is not impacted. The following program demonstrates this:
+    */
+    int newVariable{ 5 };
+
+    {
+        int& ref_newVariable{ newVariable }; // ref is a reference to newVariable
+        std::cout << ref_newVariable << '\n'; // prints value of ref (5)
+    } // ref is destroyed here -- newVariable is unaware of this
+
+    std::cout << newVariable << '\n'; // prints value of newVariable (5)
+
+    // When ref dies, variable x carries on as normal, blissfully unaware that a reference to it has been destroyed.
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Dangling references" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    When an object being referenced is destroyed before a reference to it, the reference is left referencing an object that no 
+    longer exists. Such a reference is called a dangling reference. Accessing a dangling reference leads to undefined behavior.
+
+    Dangling references are fairly easy to avoid, but we’ll show a case where this can happen in practice in lesson 9.5 -- Pass 
+    by lvalue reference.
+    */
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "References aren’t objects" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    Perhaps surprisingly, references are not objects in C++. A reference is not required to exist or occupy storage. If 
+    possible, the compiler will optimize references away by replacing all occurrences of a reference with the referent. 
+    However, this isn’t always possible, and in such cases, references may require storage.
+
+    This also means that the term “reference variable” is a bit of a misnomer, as variables are objects with a name, and 
+    references aren’t objects.
+
+    Because references aren’t objects, they can’t be used anywhere an object is required (e.g. you can’t have a reference to a 
+    reference, since an lvalue reference must reference an identifiable object). In cases where you need a reference that is an 
+    object or a reference that can be reseated, std::reference_wrapper (which we cover in lesson 16.3 -- Aggregation) provides a 
+    solution.
+
+    As an aside…
+
+    Consider the following variables:
+    */
+
+    int var{};
+    int& ref_var{ var }; // an lvalue reference bound to var
+    int& ref_ref_var{ ref_var }; // an lvalue reference bound to var
     
-    
-    
-    
+    /*
+    Because ref2 (a reference) is initialized with ref1 (a reference), you might be tempted to conclude that ref2 is a 
+    reference to a reference. It is not. Because ref1 is a reference to var, when used in an expression (such as an initializer), 
+    ref1 evaluates to var. So ref2 is just a normal lvalue reference (as indicated by its type int&), bound to var.
+
+    A reference to a reference (to an int) would have syntax int&& -- but since C++ doesn’t support references to references, 
+    this syntax was repurposed in C++11 to indicate an rvalue reference (which we cover in lesson M.2 -- R-value references).
+    */
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Quiz Time" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    Question #1
+
+    Determine what values the following program prints by yourself (do not compile the program).
+
+    #include <iostream>
+
+    int main()
+    {
+        int x{ 1 };
+        int& ref{ x };
+
+        std::cout << x << ref; // print 11
+
+        int y{ 2 };
+        ref = y;
+        y = 3;
+
+        std::cout << x << ref; // print 33
+
+        x = 4;
+
+        std::cout << x << ref; // print 44
+
+        return 0;
+    }
+    */
+
+    int x_Quiz{ 1 };
+    int& ref_Quiz{ x_Quiz };
+
+    std::cout << x_Quiz << ref_Quiz; // print 11
+
+    int y_Quiz{ 2 };
+    ref_Quiz = y_Quiz;
+    y_Quiz = 3;
+
+    std::cout << x_Quiz << ref_Quiz; // print 33
+
+    x_Quiz = 4;
+
+    std::cout << x_Quiz << ref_Quiz; // print 44
+
+    /*
+    112244
+
+    Because ref is bound to x, x and ref are synonymous, so they will always print the same value. The line ref = y assigns 
+    the value of y (2) to ref -- it does not change ref to reference y. The subsequent line y = 3 only changes y.
+    */
     
     
     
     
     
     return 0;
-}
+}// i_One and ref_i_One die here
+// newVariable destroyed here
