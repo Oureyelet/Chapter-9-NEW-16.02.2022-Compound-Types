d7a68d9af211c83bdc86c996150b576c874ad91a
Mateusz <oureyelet@gmail.com>
2022-03-22 19:22:59 +0000

    9.x — Chapter 9 summary and quiz (vsCode)

--- 9.x — Chapter 9 summary and quiz (vsCode)/main.cpp
+++ 9.x — Chapter 9 summary and quiz (vsCode)/main.cpp
@@ -19,9 +19,22 @@ struct Monster
     int hp{};
 };
 
-void printMonster()
+void printMonster(Monster_type type, Monster creatura)
 {
-    
+    std::cout << "This ";
+
+    switch (type)
+    {
+    case Monster_type::ogre: std::cout << "Ogre"; break;
+    case Monster_type::dragon: std::cout <<  "Dragon"; break;
+    case Monster_type::orc: std::cout <<  "Orc"; break;
+    case Monster_type::spider: std::cout <<  "Spider"; break;
+    case Monster_type::slime: std::cout <<  "Slime"; break;
+
+    default: std::cout <<  "???"; break;
+    }
+
+    std::cout << " is named " << creatura.name << " and has " << creatura.hp << " health.\n";
 }
 
 
@@ -43,7 +56,64 @@ int main()
     This Ogre is named Torg and has 145 health.
     This Slime is named Blurp and has 23 health.
     */
+    Monster ogre{ "Torg", 145 };
+    printMonster(Monster_type::ogre, ogre);
 
+    Monster slime{ "Blurp", 23 };
+    printMonster(Monster_type::slime, slime);
+
+
+    /*
+    Question #2
+
+    Specify whether objects of each of the given types should be passed by value, const address, or const reference. 
+    You can assume the function that takes these types as parameters doesn’t modify them.
+
+    a) char
+
+    Show Solution
+
+    b) std::string
+
+    Show Solution
+
+    c) unsigned long
+
+    Show Solution
+
+    d) bool
+
+    Show Solution
+
+    e) An enumerated type
+
+    Show Solution
+
+    f)
+
+    struct Position
+    {
+    double x{};
+    double y{};
+    double z{};
+    };
+
+    Show Solution
+
+    g)
+
+    struct Player
+    {
+    int health{};
+    // The Player struct is still under development.  More members will be added.
+    };
+
+    Show Solution
+
+    h) an object whose address is held in an int*
+
+    Show Solution
+    */
 
     return 0;
 }
