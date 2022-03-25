5e9f61bb6375e14224203e55033976a8b2346953
Mateusz <oureyelet@gmail.com>
2022-03-25 05:27:09 +0000

    9.y — Using a language reference (vsCode) + quiz

--- 9.y — Using a language reference (vsCode)/main.cpp
+++ 9.y — Using a language reference (vsCode)/main.cpp
@@ -35,5 +35,70 @@ int main()
     }
 
 
+    //Quiz time:
+    /*
+    Question #1
+
+    What does the following program print? Don’t run it, use a reference to figure out what erase does.
+
+    #include <iostream>
+    #include <string>
+
+    int main()
+    {
+    std::string str{ "The rice is cooking" };
+
+    str.erase(4, 11);
+
+    std::cout << str << '\n';
+
+    return 0;
+    }
+
+    Tip
+
+    When you find erase on cppreference, you can ignore the function signatures (2) and (3).
+
+    Tip
+
+    Indexes in C++ start at 0. The character at index 0 in the string “House” is ‘H’, at 1 it’s ‘o’, and so on.
+    */
+    std::string str{ "The rice is cooking" };
+
+    str.erase(4, 11);
+
+    std::cout << str << '\n'; // print The King
+
+    /*
+    Question #2
+
+    In the following code, modify str so that its value is “I saw a blue car yesterday” without repeating the string. 
+    For example, don’t do this:
+
+    str = "I saw a blue car yesterday.";
+
+    You only need to call one function to replace “red” with “blue”.
+
+    #include <iostream>
+    #include <string>
+
+    int main()
+    {
+    std::string str{ "I saw a red car yesterday." };
+
+    // ...
+
+    std::cout << str << '\n'; // I saw a blue car yesterday.
+
+    return 0;
+    }
+    */
+    std::string str2{ "I saw a red car yesterday." };
+
+    //str.replace(8,6,"a shorty",7);
+    str2.replace(8,3, "blue");
+
+    std::cout << str2 << '\n'; // print // I saw a blue car yesterday.  
+
     return 0;
 }
